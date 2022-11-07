df9b6d560d2fd8241e70b828a5a45c69349ecbed
Shipoto <klerk7777@mail.ru>
2022-11-07 20:13:57 +0300

    Corrected roman values to 100

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         System.out.print("Ввведите выражение для вычисления: ");
         String input = in.nextLine();
         calc(input);
-        // автоматический ввод
-//        calc("21 + 2");
+//         автоматический ввод
+//        calc("X * IX");
     }
     public static String calc(String input) throws IOException {
         String result = "";
@@ -27,7 +27,7 @@ public class Main {
         String second = inArray[2];
         String operand = inArray[1];
 
-        // Создаем hashmap для римских цифр
+        // Создаем hashmap для проверки цифр от 1 до 10
         HashMap<String, String> roman = new HashMap<>();
         roman.put("I", "1");
         roman.put("II", "2");
@@ -39,6 +39,109 @@ public class Main {
         roman.put("VIII", "8");
         roman.put("IX", "9");
         roman.put("X", "10");
+
+        // Создаем hashmap для цифр от 1 до 100
+        HashMap<String, String> romanHundered = new HashMap<>();
+        romanHundered.put("I", "1");
+        romanHundered.put("II", "2");
+        romanHundered.put("III", "3");
+        romanHundered.put("IV", "4");
+        romanHundered.put("V", "5");
+        romanHundered.put("VI", "6");
+        romanHundered.put("VII", "7");
+        romanHundered.put("VIII", "8");
+        romanHundered.put("IX", "9");
+        romanHundered.put("X", "10");
+        romanHundered.put("XI", "11");
+        romanHundered.put("XII", "12");
+        romanHundered.put("XIII", "13");
+        romanHundered.put("XIV", "14");
+        romanHundered.put("XV", "15");
+        romanHundered.put("XVI", "16");
+        romanHundered.put("XVII", "17");
+        romanHundered.put("XVIII", "18");
+        romanHundered.put("XIX", "19");
+        romanHundered.put("XX", "20");
+        romanHundered.put("XXI", "21");
+        romanHundered.put("XXII", "22");
+        romanHundered.put("XXIII", "23");
+        romanHundered.put("XXIV", "24");
+        romanHundered.put("XXV", "25");
+        romanHundered.put("XXVI", "26");
+        romanHundered.put("XXVII", "27");
+        romanHundered.put("XXVIII", "28");
+        romanHundered.put("XXIX", "29");
+        romanHundered.put("XXX", "30");
+        romanHundered.put("XXXI", "31");
+        romanHundered.put("XXXII", "32");
+        romanHundered.put("XXXIII", "33");
+        romanHundered.put("XXXIV", "34");
+        romanHundered.put("XXXV", "35");
+        romanHundered.put("XXXVI", "36");
+        romanHundered.put("XXXVII", "37");
+        romanHundered.put("XXXVIII", "38");
+        romanHundered.put("XXXIX", "39");
+        romanHundered.put("XL", "40");
+        romanHundered.put("XLI", "41");
+        romanHundered.put("XLII", "42");
+        romanHundered.put("XLIII", "43");
+        romanHundered.put("XLIV", "44");
+        romanHundered.put("XLV", "45");
+        romanHundered.put("XLVI", "46");
+        romanHundered.put("XLVII", "47");
+        romanHundered.put("XLVIII", "48");
+        romanHundered.put("XLIX", "49");
+        romanHundered.put("L", "50");
+        romanHundered.put("LI", "51");
+        romanHundered.put("LII", "52");
+        romanHundered.put("LIII", "53");
+        romanHundered.put("LIV", "54");
+        romanHundered.put("LV", "55");
+        romanHundered.put("LVI", "56");
+        romanHundered.put("LVII", "57");
+        romanHundered.put("LVIII", "58");
+        romanHundered.put("LIX", "59");
+        romanHundered.put("LX", "60");
+        romanHundered.put("LXI", "61");
+        romanHundered.put("LXII", "62");
+        romanHundered.put("LXIII", "63");
+        romanHundered.put("LXIV", "64");
+        romanHundered.put("LXV", "65");
+        romanHundered.put("LXVI", "66");
+        romanHundered.put("LXVII", "67");
+        romanHundered.put("LXVIII", "68");
+        romanHundered.put("LXIX", "69");
+        romanHundered.put("LXX", "70");
+        romanHundered.put("LXXI", "71");
+        romanHundered.put("LXXII", "72");
+        romanHundered.put("LXXIII", "73");
+        romanHundered.put("LXXIV", "74");
+        romanHundered.put("LXXV", "75");
+        romanHundered.put("LXXVI", "76");
+        romanHundered.put("LXXVII", "77");
+        romanHundered.put("LXXVIII", "78");
+        romanHundered.put("LXXIX", "79");
+        romanHundered.put("LXXX", "80");
+        romanHundered.put("LXXXI", "81");
+        romanHundered.put("LXXXII", "82");
+        romanHundered.put("LXXXIII", "83");
+        romanHundered.put("LXXXIV", "84");
+        romanHundered.put("LXXXV", "85");
+        romanHundered.put("LXXXVI", "86");
+        romanHundered.put("LXXXVII", "87");
+        romanHundered.put("LXXXVIII", "88");
+        romanHundered.put("LXXXIX", "89");
+        romanHundered.put("XC", "90");
+        romanHundered.put("XCI", "91");
+        romanHundered.put("XCII", "92");
+        romanHundered.put("XCIII", "93");
+        romanHundered.put("XCIV", "94");
+        romanHundered.put("XCV", "95");
+        romanHundered.put("XCVI", "96");
+        romanHundered.put("XCVII", "97");
+        romanHundered.put("XCVIII", "98");
+        romanHundered.put("XCIX", "99");
+        romanHundered.put("C", "100");
 
         // Проверяем корректность введеных данных
         if (roman.containsValue(first)) {
@@ -75,9 +178,10 @@ public class Main {
             // производим вычисление римских чисел
             if (operand.equals(plus)) {
                 String preresult = String.valueOf(Integer.parseInt(roman.get(first)) + Integer.parseInt(roman.get(second)));
-                for (String key : roman.keySet()) {
-                    if (roman.get(key).equals(preresult)) {
+                for (String key : romanHundered.keySet()) {
+                    if (romanHundered.get(key).equals(preresult)) {
                         result = key;
+                        System.out.println(preresult);
                         System.out.println("Результат операции римских чисел: " + result);
                     }
                 }
@@ -87,8 +191,8 @@ public class Main {
                 if (Integer.parseInt(preresult) <= 0) {
                     throw new IOException();
                 }
-                for (String key : roman.keySet()) {
-                    if (roman.get(key).equals(preresult)) {
+                for (String key : romanHundered.keySet()) {
+                    if (romanHundered.get(key).equals(preresult)) {
                         result = key;
                         System.out.println("Результат операции римских чисел: " + result);
                     }
@@ -96,17 +200,17 @@ public class Main {
 
             } else if (operand.equals(divide)) {
                 String preresult = String.valueOf(Integer.parseInt(roman.get(first)) / Integer.parseInt(roman.get(second)));
-                for (String key : roman.keySet()) {
-                    if (roman.get(key).equals(preresult)) {
+                for (String key : romanHundered.keySet()) {
+                    if (romanHundered.get(key).equals(preresult)) {
                         result = key;
                         System.out.println("Результат операции римских чисел: " + result);
                     }
                 }
 
             } else if (operand.equals(multiply)) {
-                String preresult = String.valueOf(Integer.parseInt(roman.get(first)) * Integer.parseInt(roman.get(second)));
-                for (String key : roman.keySet()) {
-                    if (roman.get(key).equals(preresult)) {
+                String preresult = String.valueOf(Integer.parseInt(romanHundered.get(first)) * Integer.parseInt(romanHundered.get(second)));
+                for (String key : romanHundered.keySet()) {
+                    if (romanHundered.get(key).equals(preresult)) {
                         result = key;
                         System.out.println("Результат операции римских чисел: " + result);
                     }
@@ -116,6 +220,7 @@ public class Main {
         } else {
             throw new IOException();
         }
+
 //        Rimlin rimlin = Rimlin.I;
 //        System.out.println(rimlin.getTranslation());
 
